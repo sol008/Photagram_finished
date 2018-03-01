280fa363b307e5b38dbef7db8b888b141e702e93
HLCPhantom1993 <chenhonglin9377@gmail.com>
2018-03-01 11:54:52 -0800

    Working react
    login / register / new password / checkout / Order Placed

---
+++ src/components/about/CheckoutPage.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {Link} from 'react-router';
+import credit from './creditcard.jpg';
+
+class CheckoutPage extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Photagram</h1>
+        <h2>Check Out</h2>
+
+        <div className="imgcontainer">
+          <img id="checkcard" src={credit} alt="credit" />
+        </div>
+
+        <div className="container">
+          <div className="column-1">
+            <label><b>Cardholder's Name</b></label>
+            <br />
+            <input type="text" placeholder="Mr/Mrs" name="cardholder" required />
+            <br />
+            <label><b>Card Number</b></label>
+            <br />
+            <input type="password" placeholder="xxxx-xxxx-xxxx-xxxx" name="cardnumber" required />
+            <br />
+            <label><b>Valid Thru</b></label>
+            <br />
+            <input type="text" placeholder="MM / YY" name="date" required />
+            <br />
+            <label><b>CVV / CVC *</b></label>
+            <br />
+            <input type="password" placeholder="Enter three digits" name="verificaiton" required />
+            <br />
+            <span className="verify">* CVV or CVC is the card security code, unique three digits number on the back of your card separate from its number.</span>
+          </div>
+        </div>
+
+        <br/> 
+
+        <div className="column-2">
+          <p><strong>Order Summary:</strong></p>
+          <br />
+          <p>The total amount is: $40</p>
+          <form action="orderPlacedPage.html">
+            <button className="orderbtn">Order</button>
+          </form>
+      </div>
+
+      </div>
+    );
+  }
+}
+
+export default CheckoutPage;
--- src/components/about/FakemainPage.js
+++ src/components/about/FakemainPage.js
@@ -6,12 +6,6 @@ var divStyle = {
   color: 'Blue'
 };
 
-// var imageName = require ('./6.jpg');
-
-
-
-
-
 class FakemainPage extends React.Component {
 
   pop  ()  {
@@ -20,139 +14,145 @@ class FakemainPage extends React.Component {
 
   render() {
     return (
-
-
-
       <div>
 
      <li><a href="Pictures.html">Photographs</a></li>
      <li><a href="Videos.html">Videos</a></li>
      <li><a href="Paintings.html">Paintings</a></li>
-                        
-
-
-    <h1 style={divStyle}>Editor's Pick</h1>
 
 
 
-   <div className="column">
+<h1 style={divStyle}>Editor's Pick</h1>
+
+
+
+<div className="column">
     <img id="myImg6"  src = {six} alt="six" className="hover-shadow cursor" />
     <a href="profiles.html">➣➣➣</a>
-  </div>
+</div>
 
 
-  <div className="column">
+<div className="column">
     <img id="myImg7"  src="7.jpg"  className="hover-shadow cursor" />
     <a href="profiles.html">➣➣➣</a>
-  </div>
+</div>
 
-  <div className="column">
+<div className="column">
     <img id="myImg8"  src="8.jpg"  className="hover-shadow cursor" />
     <a href="profiles.html">➣➣➣</a>
-  </div>
-  <div className="column">
+</div>
+
+<div className="column">
     <img id="myImg14"  src="14.jpg"  className="hover-shadow cursor" />
     <a href="profiles.html">➣➣➣</a>
-  </div>
-  <div className="column">
+</div>
+
+<div className="column">
     <img id="myImg14"  src="15.jpg"  className="hover-shadow cursor" />
     <a href="profiles.html">➣➣➣</a>
-  </div>
-<h1 >Photographs  <a href="Pictures.html" >(View All)</a></h1> 
+</div>
 
 
-     <div className="column">
+<h1 >Photographs  <a href="Pictures.html" >(View All)</a></h1>
+
+
+<div className="column">
     <img id="myImg1"  src="1.jpg"  className="hover-shadow cursor" />
     <a href="profiles.html">➣➣➣</a>
-  </div>
-  <div className="column">
+</div>
+
+<div className="column">
     <img id="myImg2"  src="2.jpg"  className="hover-shadow cursor" />
     <a href="profiles.html">➣➣➣</a>
-  </div>
-  <div className="column">
+</div>
+
+<div className="column">
     <img id="myImg3"  src="3.jpg"  className="hover-shadow cursor" />
     <a href="profiles.html">➣➣➣</a>
-  </div>
-  <div className="column">
+</div>
+
+<div className="column">
     <img id="myImg4"  src="4.jpg"  className="hover-shadow cursor" />
     <a href="profiles.html">➣➣➣</a>
-  </div>
-  <div className="column">
+</div>
+
+<div className="column">
     <img id="myImg5"  src="5.jpg" className="hover-shadow cursor" />
     <a href="profiles.html">➣➣➣</a>
-  </div>
-
-
-
-
+</div>
 
 <h1 >Videos  <a href="Videos.html" >(View All)</a></h1>
 
 <div className="row">
-  <div className="column">
+
+<div className="column">
     <iframe src="https://www.youtube.com/embed/raNzEp6S7X8" ></iframe>
     <a href="profiles.html">➣➣➣</a>
-  </div>
-
-  <div className="column">
-    <iframe src="https://www.youtube.com/embed/Ml90FJK-emo"></iframe>
-    <a href="profiles.html">➣➣➣</a>
-  </div>
-  <div className="column">
-    <iframe src="https://www.youtube.com/embed/cOtigA2G1GQ"></iframe>
-    <a href="profiles.html">➣➣➣</a>
-  </div>
-  <div className="column">
-    <iframe src="https://www.youtube.com/embed/ZCIb-R38XeU"></iframe>
-    <a href="profiles.html">➣➣➣</a>
-  </div>
-  <div className="column">
-    <iframe src="https://www.youtube.com/embed/rDLJCQpExJA"></iframe>
-    <a href="profiles.html">➣➣➣</a>
-  </div>
-
-  </div>
-
- <h1 >Paintings  <a href="Paintings.html" target="_top">(View All)</a></h1>      
-
-<div className="row">
-   <div className="column">
-    <img id="myImg11"  src = {eleven} alt="eleven"  className="hover-shadow cursor" />
-    <a href="profiles.html">➣➣➣</a>
-  </div>
-  <div className="column">
-    <img id="myImg12"  src="12.jpg"  className="hover-shadow cursor" />
-    <a href="profiles.html">➣➣➣</a>
-  </div>
-  <div className="column">
-    <img id="myImg13"  src="13.jpg"  className="hover-shadow cursor" />
-    <a href="profiles.html">➣➣➣</a>
-  </div>
-  <div className="column">
-    <img id="myImg14"  src="14.jpg" className="hover-shadow cursor" />
-    <a href="profiles.html">➣➣➣</a>
-  </div>
-  <div className="column">
-    <img id="myImg15"  src="15.jpg"  className="hover-shadow cursor" />
-    <a href="profiles.html">➣➣➣</a>
-  </div>
 </div>
 
+<div className="column">
+  <iframe src="https://www.youtube.com/embed/Ml90FJK-emo"></iframe>
+    <a href="profiles.html">➣➣➣</a>
+</div>
 
-<button onClick={this.pop.bind(this)}> 
+<div className="column">
+    <iframe src="https://www.youtube.com/embed/cOtigA2G1GQ"></iframe>
+    <a href="profiles.html">➣➣➣</a>
+</div>
+
+<div className="column">
+    <iframe src="https://www.youtube.com/embed/ZCIb-R38XeU"></iframe>
+    <a href="profiles.html">➣➣➣</a>
+</div>
+
+<div className="column">
+    <iframe src="https://www.youtube.com/embed/rDLJCQpExJA"></iframe>
+    <a href="profiles.html">➣➣➣</a>
+</div>
+
+</div>
+
+ <h1 >Paintings  <a href="Paintings.html" target="_top">(View All)</a></h1>
+
+<div className="row">
+
+<div className="column">
+    <img id="myImg11"  src = {eleven} alt="eleven"  className="hover-shadow cursor" />
+    <a href="profiles.html">➣➣➣</a>
+</div>
+
+<div className="column">
+    <img id="myImg12"  src="12.jpg"  className="hover-shadow cursor" />
+    <a href="profiles.html">➣➣➣</a>
+</div>
+
+<div className="column">
+    <img id="myImg13"  src="13.jpg"  className="hover-shadow cursor" />
+    <a href="profiles.html">➣➣➣</a>
+</div>
+
+<div className="column">
+    <img id="myImg14"  src="14.jpg" className="hover-shadow cursor" />
+    <a href="profiles.html">➣➣➣</a>
+</div>
+
+<div className="column">
+    <img id="myImg15"  src="15.jpg"  className="hover-shadow cursor" />
+    <a href="profiles.html">➣➣➣</a>
+</div>
+
+</div>
+
+<button onClick={this.pop.bind(this)}>
   About us
 </button>
 
-  <p>Copyright &copy;photagram.com</p>
+<p>Copyright &copy;photagram.com</p>
+
 </div>
 
-
-
-
 );
-  }
-
+}
 }
 
 export default FakemainPage;
-
